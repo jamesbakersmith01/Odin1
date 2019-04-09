04cb8266a36eb18bf29f311edd37bf3f8d2258c9
jamesbakersmith01 <jamesbakersmith5501@outlook.com>
2019-04-09 17:43:31 -0400

    Updates

---
+++ precache-manifest.fd551d4c73b318bef9f1c3544c265f88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "06e89ac88cedd0e8b305",
+    "url": "/Odin1/static/css/main.6d7290ae.chunk.css"
+  },
+  {
+    "revision": "06e89ac88cedd0e8b305",
+    "url": "/Odin1/static/js/main.0010d387.chunk.js"
+  },
+  {
+    "revision": "d74f3055bc5a30a92fff",
+    "url": "/Odin1/static/js/runtime~main.87528221.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Odin1/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Odin1/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "ce15e7499edecceaf4869ef1a0429630",
+    "url": "/Odin1/index.html"
+  }
+];
